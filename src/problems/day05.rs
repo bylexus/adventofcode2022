55e4fdf266e3c4989a678af0fe8b9e7154bc5332
Alexander Schenkel <aschenkel@kadenpartner.ch>
2022-12-05 16:00:15 +0100

    Day 05, optimized. Runs in 0.5ms.

--- src/problems/day05.rs
+++ src/problems/day05.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, VecDeque},
-    slice::SliceIndex,
-};
+use std::collections::HashMap;
 
 use regex::Regex;
 
@@ -15,8 +12,8 @@ struct Instr {
 }
 
 pub struct Day05 {
-    stacks: HashMap<usize, VecDeque<char>>,
-    stacks2: HashMap<usize, VecDeque<char>>,
+    stacks1: HashMap<usize, Vec<char>>,
+    stacks2: HashMap<usize, Vec<char>>,
     instructions: Vec<Instr>,
     solution1: String,
     solution2: String,
@@ -25,7 +22,7 @@ pub struct Day05 {
 impl Day05 {
     pub fn new() -> Day05 {
         Day05 {
-            stacks: HashMap::new(),
+            stacks1: HashMap::new(),
             stacks2: HashMap::new(),
             instructions: Vec::new(),
             solution1: String::new(),
@@ -38,6 +35,7 @@ impl Problem for Day05 {
     fn setup(&mut self) {
         // let lines = crate::read_lines("input-data/05-test.txt");
         let lines = crate::read_lines("input-data/05-data.txt");
+
         // find empty line:
         let mut idx = 0;
         for i in &lines {
@@ -47,31 +45,25 @@ impl Problem for Day05 {
             idx += 1;
         }
         idx -= 1;
-        // println!("idx: {}", idx);
-        // count nr of stacks: last line
         let nr_of_stacks = (lines[idx].len() + 1) / 4;
-        // println!("Nr of stacks: {}", nr_of_stacks);
 
         for i in 1..=nr_of_stacks {
-            self.stacks.insert(i, VecDeque::new());
-            self.stacks2.insert(i, VecDeque::new());
+            // create 2 separate storates for problem 1 + 2, as we mofify the input
+            // during the solution:
+            self.stacks1.insert(i, Vec::with_capacity(100));
+            self.stacks2.insert(i, Vec::with_capacity(100));
         }
 
         // extract stacks
-        for line_ids in 0..idx {
+        for line_ids in (0..idx).rev() {
             let line = lines.get(line_ids).unwrap();
+            // parse lines from bottom to up, add it to the stacks:
             for stack_idx in 0..nr_of_stacks {
                 let chars: Vec<char> = line.chars().collect();
                 let chr = chars.get(stack_idx * 4 + 1).unwrap();
                 if *chr != '\u{0020}' {
-                    self.stacks
-                        .get_mut(&(stack_idx + 1))
-                        .unwrap()
-                        .push_back(*chr);
-                    self.stacks2
-                        .get_mut(&(stack_idx + 1))
-                        .unwrap()
-                        .push_back(*chr);
+                    self.stacks1.get_mut(&(stack_idx + 1)).unwrap().push(*chr);
+                    self.stacks2.get_mut(&(stack_idx + 1)).unwrap().push(*chr);
                 }
             }
         }
@@ -92,13 +84,6 @@ impl Problem for Day05 {
                 to: str::parse(groups.get(3).unwrap().as_str()).unwrap(),
             });
         }
-        // println!("Instructions: {:?}", self.instructions);
-
-        // let lines = crate::read_lines("input-data/05-data.txt");
-
-        crate::split_lines(&lines, " ").iter().for_each(|parts| {});
-        self.solution1 = String::new();
-        self.solution2 = String::new();
     }
 
     fn title(&self) -> String {
@@ -107,50 +92,37 @@ impl Problem for Day05 {
 
     fn solve_problem1(&mut self) {
         for instr in &self.instructions {
-            // println!("\n\nStacks: {:?}", self.stacks);
-            // println!("Instr: {:?}", instr);
             for _ in 0..instr.amount {
-                let item = self
-                    .stacks
-                    .get_mut(&instr.from)
-                    .unwrap()
-                    .pop_front()
-                    .unwrap();
-                self.stacks.get_mut(&instr.to).unwrap().push_front(item);
+                let item = self.stacks1.get_mut(&instr.from).unwrap().pop().unwrap();
+                self.stacks1.get_mut(&instr.to).unwrap().push(item);
             }
-            // println!("Stacks after: {:?}", self.stacks);
         }
 
-        for i in 1..=(self.stacks.len()) {
+        for i in 1..=(self.stacks1.len()) {
             self.solution1
-                .push(*self.stacks.get(&i).unwrap().get(0).unwrap());
+                .push(*self.stacks1.get(&i).unwrap().last().unwrap());
         }
     }
 
     fn solve_problem2(&mut self) {
-        let mut tmp: VecDeque<char> = VecDeque::with_capacity(100);
+        let mut tmp: Vec<char> = Vec::with_capacity(100);
         for instr in &self.instructions {
-            tmp.clear();
-            // println!("\n\nStacks: {:?}", self.stacks2);
-            // println!("Instr: {:?}", instr);
             for _ in 0..instr.amount {
-                let item = self
-                    .stacks2
-                    .get_mut(&instr.from)
+                let item = self.stacks2.get_mut(&instr.from).unwrap().pop().unwrap();
+                tmp.push(item);
+            }
+            while tmp.len() > 0 {
+                self.stacks2
+                    .get_mut(&instr.to)
                     .unwrap()
-                    .pop_front()
-                    .unwrap();
-                tmp.push_back(item);
+                    .push(tmp.pop().unwrap());
             }
-            for i in tmp.iter().rev() {
-                self.stacks2.get_mut(&instr.to).unwrap().push_front(*i);
-            }
-            // println!("Stacks after: {:?}", self.stacks2);
+            tmp.clear();
         }
 
         for i in 1..=(self.stacks2.len()) {
             self.solution2
-                .push(*self.stacks2.get(&i).unwrap().get(0).unwrap());
+                .push(*self.stacks2.get(&i).unwrap().last().unwrap());
         }
     }
 
