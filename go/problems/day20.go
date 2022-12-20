e626976883e5485aa9699491e245edf6a7f61024
Alexander Schenkel <aschenkel@kadenpartner.ch>
2022-12-20 22:17:41 +0100

    Day 20 - optimized solution for both 1 + 2

--- go/problems/day20.go
+++ go/problems/day20.go
@@ -10,6 +10,8 @@ import (
 type Number20 struct {
 	nr       int64
 	orig_pos int64
+	prev     *Number20
+	next     *Number20
 }
 
 func (n Number20) String() string {
@@ -34,14 +36,38 @@ func (d *Day20) Title() string {
 func (d *Day20) Setup() {
 	// var lines = lib.ReadLines("data/20-test.txt")
 	var lines = lib.ReadLines("data/20-data.txt")
+	var prev1 *Number20 = nil
+	var prev2 *Number20 = nil
 	for i, line := range lines {
 		if len(line) > 0 {
 			nr, err := strconv.ParseInt(line, 10, 64)
 			lib.Check(err)
-			d.numbers = append(d.numbers, &Number20{nr: nr, orig_pos: int64(i)})
-			d.numbers2 = append(d.numbers2, &Number20{nr: nr * 811589153, orig_pos: int64(i)})
+			var nr1 = Number20{nr: nr, orig_pos: int64(i)}
+			var nr2 = Number20{nr: nr * 811589153, orig_pos: int64(i)}
+			if prev1 != nil {
+				prev1.next = &nr1
+				nr1.prev = prev1
+			}
+			if prev2 != nil {
+				prev2.next = &nr2
+				nr2.prev = prev2
+			}
+
+			prev1 = &nr1
+			prev2 = &nr2
+
+			d.numbers = append(d.numbers, &nr1)
+			d.numbers2 = append(d.numbers2, &nr2)
 		}
 	}
+	// form ring:
+	d.numbers[0].prev = d.numbers[len(d.numbers)-1]
+	d.numbers[len(d.numbers)-1].next = d.numbers[0]
+
+	d.numbers2[0].prev = d.numbers2[len(d.numbers2)-1]
+	d.numbers2[len(d.numbers2)-1].next = d.numbers2[0]
+
+	// fmt.Println(d.ringStr(d.numbers[0]))
 	// fmt.Printf("%v\n", d.numbers)
 }
 
@@ -49,67 +75,32 @@ func (d *Day20) SolveProblem1() {
 
 	/**
 	 * Ring buffer problem:
-	 * I take a simple array as ring buffer.
-	 * Moving an element is done the following way:
-	 * - remove the element from the array
-	 * - calculate the new insert position. This can easily be done using mod() as ring wrapper function
-	 * - insert removed element at the new position
 	 *
-	 * This is done by slicing / re-creating the array. Maybe I will redo it using a double-linked list.
+	 * The actual solution takes a real ring buffer, formed with
+	 * a double linked list.
+	 *
+	 * I just move the one element n mod list length, instead of the full length.
+	 * This way I can save a loooot of movements.
 	 */
 
-	var length = int64(len(d.numbers))
-	// fmt.Printf("Length: %d\n", length)
-	var insert_idx int64 = 0
-
-	for i := int64(0); i < length; i++ {
-		// find number by searching the orig_pos:
-		var start_idx = d.findIndexOfOrigPos(i, &d.numbers)
-		var element = d.numbers[start_idx]
-
-		// fmt.Printf("before move: %v\n", d.numbers)
-		// fmt.Printf("moving Nr: %d\n", element.nr)
-
-		if element.nr == 0 {
-			continue
-		}
-
-		// remove actual element from list:
-		var tmp = make([]*Number20, 0)
-		tmp = append(tmp, d.numbers[:start_idx]...)
-		tmp = append(tmp, d.numbers[start_idx+1:]...)
-		// fmt.Printf("after remove: %v\n", tmp)
-
-		// calc insert pos:
-		if element.nr > 0 {
-			insert_idx = (start_idx + element.nr - 1) % (length - 1)
-			// insert AFTER insert_idx:
-			var tmp2 = make([]*Number20, 0)
-			tmp2 = append(tmp2, tmp[:insert_idx+1]...)
-			tmp2 = append(tmp2, element)
-			tmp2 = append(tmp2, tmp[insert_idx+1:]...)
-			tmp = tmp2
-		} else {
-			// --> (len + (start + nr) % len) % 6
-			var l = length - 1
-			insert_idx = (l + (start_idx+element.nr-1)%l) % l
-			// insert AFTER insert_idx:
-			var tmp2 = make([]*Number20, 0)
-			tmp2 = append(tmp2, tmp[:insert_idx+1]...)
-			tmp2 = append(tmp2, element)
-			tmp2 = append(tmp2, tmp[insert_idx+1:]...)
-			tmp = tmp2
-		}
-
-		d.numbers = tmp
-		// fmt.Printf("after move: %v\n\n", d.numbers)
-	}
+	d.moveAllNumbersOnce(&d.numbers)
+	var length int64 = int64(len(d.numbers))
 
 	// fmt.Printf("after move: %v\n\n", d.numbers)
-	var zero_idx = d.findIndexOfNr(0, &d.numbers)
-	var nr_1000 = d.numbers[(zero_idx+1000)%int64(len(d.numbers))].nr
-	var nr_2000 = d.numbers[(zero_idx+2000)%int64(len(d.numbers))].nr
-	var nr_3000 = d.numbers[(zero_idx+3000)%int64(len(d.numbers))].nr
+	var idx = d.findIndexOfNr(0, &d.numbers)
+	var act = d.numbers[idx]
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_1000 = act.nr
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_2000 = act.nr
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_3000 = act.nr
 	fmt.Printf("1000: %d, 2000: %d, 3000: %d\n", nr_1000, nr_2000, nr_3000)
 
 	d.s1 = nr_1000 + nr_2000 + nr_3000
@@ -122,58 +113,27 @@ func (d *Day20) SolveProblem2() {
 	 */
 	var length = int64(len(d.numbers2))
 	// fmt.Printf("Length: %d\n", length)
-	var insert_idx int64 = 0
 
 	for r := 0; r < 10; r++ {
-		for i := int64(0); i < length; i++ {
-			// find number by searching the orig_pos:
-			var start_idx = d.findIndexOfOrigPos(i, &d.numbers2)
-			var element = d.numbers2[start_idx]
-
-			// fmt.Printf("before move: %v\n", d.numbers2)
-			// fmt.Printf("moving Nr: %d\n", element.nr)
-
-			if element.nr == 0 {
-				continue
-			}
-
-			// remove actual element from list:
-			var tmp = make([]*Number20, 0)
-			tmp = append(tmp, d.numbers2[:start_idx]...)
-			tmp = append(tmp, d.numbers2[start_idx+1:]...)
-			// fmt.Printf("after remove: %v\n", tmp)
-
-			// calc insert pos:
-			if element.nr > 0 {
-				insert_idx = (start_idx + element.nr - 1) % (length - 1)
-				// insert AFTER insert_idx:
-				var tmp2 = make([]*Number20, 0)
-				tmp2 = append(tmp2, tmp[:insert_idx+1]...)
-				tmp2 = append(tmp2, element)
-				tmp2 = append(tmp2, tmp[insert_idx+1:]...)
-				tmp = tmp2
-			} else {
-				// --> (len + (start + nr) % len) % 6
-				var l = length - 1
-				insert_idx = (l + (start_idx+element.nr-1)%l) % l
-				// insert AFTER insert_idx:
-				var tmp2 = make([]*Number20, 0)
-				tmp2 = append(tmp2, tmp[:insert_idx+1]...)
-				tmp2 = append(tmp2, element)
-				tmp2 = append(tmp2, tmp[insert_idx+1:]...)
-				tmp = tmp2
-			}
-
-			d.numbers2 = tmp
-			// fmt.Printf("after move: %v\n\n", d.numbers)
-		}
+		d.moveAllNumbersOnce(&d.numbers2)
 	}
 
-	// fmt.Printf("after move: %v\n\n", d.numbers2)
-	var zero_idx = d.findIndexOfNr(0, &d.numbers2)
-	var nr_1000 = d.numbers2[(zero_idx+1000)%length].nr
-	var nr_2000 = d.numbers2[(zero_idx+2000)%length].nr
-	var nr_3000 = d.numbers2[(zero_idx+3000)%length].nr
+	// fmt.Printf("after move: %v\n\n", d.numbers)
+	var idx = d.findIndexOfNr(0, &d.numbers2)
+	var act = d.numbers2[idx]
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_1000 = act.nr
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_2000 = act.nr
+	for i := int64(0); i < 1000%length; i++ {
+		act = act.next
+	}
+	var nr_3000 = act.nr
+	fmt.Printf("1000: %d, 2000: %d, 3000: %d\n", nr_1000, nr_2000, nr_3000)
 	fmt.Printf("1000: %d, 2000: %d, 3000: %d\n", nr_1000, nr_2000, nr_3000)
 
 	d.s2 = nr_1000 + nr_2000 + nr_3000
@@ -185,6 +145,53 @@ func (d *Day20) Solution1() string {
 
 func (d *Day20) Solution2() string {
 	return fmt.Sprintf("%d", d.s2)
+}
+
+/**
+ * Ring buffer rotation function
+ * Rotates all numbers in order once
+ */
+func (d *Day20) moveAllNumbersOnce(numbers *[]*Number20) {
+	var length = int64(len(*numbers))
+
+	for i := int64(0); i < length; i++ {
+		// find number by searching the orig_pos:
+		var start_idx = d.findIndexOfOrigPos(i, numbers)
+		var element = (*numbers)[start_idx]
+
+		if element.nr == 0 {
+			continue
+		}
+
+		// remove actual element from list:
+		element.prev.next = element.next
+		element.next.prev = element.prev
+
+		// calc insert pos:
+		var act = element
+
+		if element.nr > 0 {
+			act = act.prev
+			for s := int64(0); s < (element.nr % (length - 1)); s++ {
+				act = act.next
+			}
+			// insert element again:
+			element.prev = act
+			element.next = act.next
+			act.next.prev = element
+			act.next = element
+		} else {
+			act = act.next
+			for s := int64(0); s > (element.nr % (length - 1)); s-- {
+				act = act.prev
+			}
+			// insert element again:
+			element.next = act
+			element.prev = act.prev
+			act.prev.next = element
+			act.prev = element
+		}
+	}
 }
 
 func (d *Day20) findIndexOfOrigPos(pos int64, numbers *[]*Number20) int64 {
@@ -203,4 +210,17 @@ func (d *Day20) findIndexOfNr(nr int64, numbers *[]*Number20) int64 {
 		}
 	}
 	panic("nr not found at original pos - cannot be!")
+}
+
+func (d *Day20) ringStr(start *Number20) string {
+	var out = ""
+	var act = start
+	for {
+		out += fmt.Sprintf("%v ", act.nr)
+		act = act.next
+		if act == start {
+			break
+		}
+	}
+	return out
 }
