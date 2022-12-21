9bcaa301a9f5d4942b63ed2036d6215caba7ba35
Alexander Schenkel <aschenkel@kadenpartner.ch>
2022-12-21 19:49:30 +0100

    Day 21 optimized to work with whole numbers, and still get the correct, single solution

--- go/problems/day21.go
+++ go/problems/day21.go
@@ -9,43 +9,49 @@ import (
 )
 
 type Expression21 interface {
-	value() int64
+	value() (int64, bool)
 	needsMonkey(string) bool
 }
 
 type Operation21 struct {
-	op        rune
-	left      string
-	right     string
-	memoized  bool
-	mem_value int64
-	monkeys   *Monkeys21
+	op      rune
+	left    string
+	right   string
+	monkeys *Monkeys21
 }
 
-func (this *Operation21) value() int64 {
-	if this.memoized {
-		fmt.Println("Using memoized value")
-		return this.mem_value
-	}
+func (this *Operation21) value() (int64, bool) {
+	// Calculate the value of this operation:
+	// Make sure the value is valid, by
+	// checking the validity of sub-operations,
+	// and to test division
 	var left = (*this.monkeys)[this.left]
 	var right = (*this.monkeys)[this.right]
 
+	lval, ok := left.value()
+	if ok == false {
+		return 0, false
+	}
+	rval, ok := right.value()
+	if ok == false {
+		return 0, false
+	}
 	switch this.op {
 	case '+':
-		this.mem_value = left.value() + right.value()
+		return lval + rval, true
 	case '-':
-		this.mem_value = left.value() - right.value()
+		return lval - rval, true
 	case '*':
-		this.mem_value = left.value() * right.value()
+		return lval * rval, true
 	case '/':
-		this.mem_value = left.value() / right.value()
+		// only valid if division is possible without remainder:
+		if lval%rval != 0 {
+			return 0, false
+		}
+		return lval / rval, true
 	default:
 		panic("Unknown operation!")
 	}
-	// this.memoized = true
-	// do not memoize for the moment:
-	this.memoized = false
-	return this.mem_value
 }
 
 func (this *Operation21) needsMonkey(name string) bool {
@@ -64,8 +70,8 @@ type Value21 struct {
 	nr int64
 }
 
-func (this *Value21) value() int64 {
-	return this.nr
+func (this *Value21) value() (int64, bool) {
+	return this.nr, true
 }
 
 func (this *Value21) needsMonkey(name string) bool {
@@ -76,8 +82,8 @@ type HumnValue21 struct {
 	nr int64
 }
 
-func (this *HumnValue21) value() int64 {
-	return this.nr
+func (this *HumnValue21) value() (int64, bool) {
+	return this.nr, true
 }
 
 func (this *HumnValue21) needsMonkey(name string) bool {
@@ -118,18 +124,16 @@ func (d *Day21) Setup() {
 		if len(opGroup) == 5 {
 			var name = opGroup[1]
 			var op1 = Operation21{
-				op:       rune(opGroup[3][0]),
-				left:     opGroup[2],
-				right:    opGroup[4],
-				memoized: false,
-				monkeys:  &d.monkeys,
+				op:      rune(opGroup[3][0]),
+				left:    opGroup[2],
+				right:   opGroup[4],
+				monkeys: &d.monkeys,
 			}
 			var op2 = Operation21{
-				op:       rune(opGroup[3][0]),
-				left:     opGroup[2],
-				right:    opGroup[4],
-				memoized: false,
-				monkeys:  &d.monkeys2,
+				op:      rune(opGroup[3][0]),
+				left:    opGroup[2],
+				right:   opGroup[4],
+				monkeys: &d.monkeys2,
 			}
 			d.monkeys[name] = &op1
 			d.monkeys2[name] = &op2
@@ -140,20 +144,20 @@ func (d *Day21) Setup() {
 			lib.Check(err)
 			if name == "humn" {
 				var val1 = HumnValue21{
-					nr: nr,
+					nr: int64(nr),
 				}
 				var val2 = HumnValue21{
-					nr: nr,
+					nr: int64(nr),
 				}
 				d.monkeys[name] = &val1
 				d.monkeys2[name] = &val2
 
 			} else {
 				var val1 = Value21{
-					nr: nr,
+					nr: int64(nr),
 				}
 				var val2 = Value21{
-					nr: nr,
+					nr: int64(nr),
 				}
 				d.monkeys[name] = &val1
 				d.monkeys2[name] = &val2
@@ -166,7 +170,11 @@ func (d *Day21) Setup() {
 func (d *Day21) SolveProblem1() {
 	var rootMonkey = d.monkeys["root"]
 
-	d.s1 = rootMonkey.value()
+	res, ok := rootMonkey.value()
+	if ok != true {
+		panic("Oops, something went completely wrong ...")
+	}
+	d.s1 = res
 }
 
 func (d *Day21) SolveProblem2() {
@@ -180,6 +188,7 @@ func (d *Day21) SolveProblem2() {
 
 	// calc the side that can be calculated:
 	var res int64 = 0
+	var ok bool
 	var humnMonkey = d.monkeys2["humn"].(*HumnValue21)
 	var fixedMonkey Expression21
 	var guessMonkey Expression21
@@ -192,7 +201,10 @@ func (d *Day21) SolveProblem2() {
 	} else {
 		panic("both monkeys need humn, not possible")
 	}
-	res = fixedMonkey.value()
+	res, ok = fixedMonkey.value()
+	if ok != true {
+		panic("Oops, this should not happen: this side should not return a false result.")
+	}
 
 	// Use a Bisect to find the correct solution:
 	// after some fiddling, I noticed that
@@ -202,24 +214,27 @@ func (d *Day21) SolveProblem2() {
 
 	// NOTE that this ONLY works with MY real data! It does not even work with the
 	// test data.....
-	// Strangely, I also get 6 possible solutions... Maybe this is because I use ints instead of floats....?
 	var upperBound int64 = 1000000000000000
-	// var upperBound int64 = 301
 	var lowerBound int64 = 0
 	var humnValue int64 = 0
-	// var maxHumn int64 = 1000000000000000
 
-	fmt.Printf("target value: %d\n", res)
 	for {
 		humnMonkey.setValue(humnValue)
-		var guess = guessMonkey.value()
-		if guess == res {
-			break
-		}
+		guess, ok := guessMonkey.value()
 		if guess > res {
+			if ok != true {
+				humnValue += 1
+				continue
+			}
 			lowerBound = humnValue
-		} else {
+		} else if guess < res {
+			if ok != true {
+				humnValue -= 1
+				continue
+			}
 			upperBound = humnValue
+		} else if guess == res {
+			break
 		}
 		if upperBound == lowerBound {
 			fmt.Println("no solution found")
@@ -228,32 +243,7 @@ func (d *Day21) SolveProblem2() {
 		humnValue = (upperBound + lowerBound) / 2
 	}
 
-	// search correct solution: multiple humn values lead to the SAME result:
-	// take the lowest:
-	// search within a range of 40 numbers around the found one:
-	var minSolution = humnValue
-	for i := int64(-20); i <= 20; i++ {
-		var try = humnValue + i
-		humnMonkey.setValue(try)
-		if leftMonkey.value() == rightMonkey.value() && try < minSolution {
-			minSolution = try
-		}
-		// fmt.Printf("val: %d, left: %d, right: %d\n", try, leftMonkey.value(), rightMonkey.value())
-	}
-	d.s2 = minSolution
-	/**
-		 got multiple valid solutions:
-	val: 3305669217840, left: 17009151241519, right: 17009151241519
-	val: 3305669217841, left: 17009151241519, right: 17009151241519
-	val: 3305669217842, left: 17009151241519, right: 17009151241519 not correct
-	val: 3305669217843, left: 17009151241519, right: 17009151241519 not correct
-	val: 3305669217844, left: 17009151241519, right: 17009151241519 too high
-	val: 3305669217845, left: 17009151241519, right: 17009151241519 too high
-		// humnMonkey.setValue(humnValue + 3)
-		humnMonkey.setValue(humnValue - 2)
-		fmt.Printf("left: %d, right: %d\n", leftMonkey.value(), rightMonkey.value())
-	*/
-
+	d.s2 = humnValue
 }
 
 func (d *Day21) Solution1() string {
